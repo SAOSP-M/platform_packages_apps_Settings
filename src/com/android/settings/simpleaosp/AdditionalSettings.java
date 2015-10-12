1cd449aea38b0881e91905fba90e071b377d8186
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-12 03:32:13 +0100

    Quick settings: Option to disable brightness slider (2/2)
    - Add notification drawer settings and add it as a fragment
    Change-Id: I0440351bc88ddcd30d2e9d22f14e3a04406ba3f3
    Signed-off-by: Altaf-Mahdi <altaf.mahdi@gmail.com>

--- src/com/android/settings/simpleaosp/AdditionalSettings.java
+++ src/com/android/settings/simpleaosp/AdditionalSettings.java
@@ -19,6 +19,7 @@ import android.view.ViewGroup;
 
 import com.android.settings.simpleaosp.StatusBarSettings;
 import com.android.settings.simpleaosp.LockScreenSettings;
+import com.android.settings.cyanogenmod.NotificationDrawerSettings;
 import com.android.settings.simpleaosp.PagerSlidingTabStrip;
 import com.android.settings.R;
 import com.android.settings.Utils;
@@ -81,6 +82,7 @@ public class AdditionalSettings extends SettingsPreferenceFragment {
         public StatusBarAdapter(FragmentManager fm) {
             super(fm);
 	    frags[0] = new StatusBarSettings();
+	    frags[1] = new NotificationDrawerSettings();
           
         }
 
@@ -103,7 +105,8 @@ public class AdditionalSettings extends SettingsPreferenceFragment {
     private String[] getTitles() {
         String titleString[];
         titleString = new String[]{
-		    getString(R.string.status_bar_title)};
+		    getString(R.string.status_bar_title)
+		    getString(R.string.notification_drawer_title)}
         return titleString;
     }
 
