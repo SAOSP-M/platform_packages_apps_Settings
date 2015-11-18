f540d7a46ad3a64be6f868b69472439d184468fa
cphelps76 <cphelps76@droidconcepts.com>
2015-11-18 12:03:37 +0000

    BatteryBar [2/2]
    Change-Id: I1297abeef9f64dc520f18827c7b6c7f331d06724
    
    Open from settings/battery/menu dropdown button

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -123,4 +123,5 @@ public class Settings extends SettingsActivity {
     public static class DisplayRotationSettingsActivity extends SettingsActivity { /* empty */ }
     public static class BlacklistSettingsActivity extends SettingsActivity { /* empty */ }
     public static class QSSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class BatteryBarSettingsActivity extends SettingsActivity { /* empty */ }
 }
--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -81,6 +81,7 @@ import com.android.settings.applications.WriteSettingsDetails;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.simpleaosp.DisplayRotation;
+import com.android.settings.simpleaosp.BatteryBarSettings;
 import com.android.settings.cyanogenmod.qs.QSTiles;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -361,7 +362,8 @@ public class SettingsActivity extends Activity
             WriteSettingsDetails.class.getName(),
             DisplayRotation.class.getName(),          
             BlacklistSettings.class.getName(),
- 	    QSTiles.class.getName()
+ 	    QSTiles.class.getName(),
+            BatteryBarSettings.class.getName()
     };
 
 
