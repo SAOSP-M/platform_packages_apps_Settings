83d2d637cd823b47beba45e5f2e95eda65b8a19f
snak3ater <saeedyasir91@gmail.com>
2015-12-20 21:31:16 +0500

    Settings: Bring back Slim clock and date customizations [2/2]

--- src/com/android/settings/simpleaosp/StatusBarSettings.java
+++ src/com/android/settings/simpleaosp/StatusBarSettings.java
@@ -39,9 +39,11 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     private static final String QUICK_PULLDOWN = "quick_pulldown";
     private static final String KEY_LOCK_CLOCK = "lock_clock";
     private static final String KEY_LOCK_CLOCK_PACKAGE_NAME = "com.cyanogenmod.lockclock";
+    private static final String KEY_STATUS_BAR_CLOCK = "clock_style_pref";
 
     private ListPreference mQuickPulldown;
     private PreferenceScreen mLockClock;
+    private PreferenceScreen mClockStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -67,6 +69,9 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         if (!Utils.isPackageInstalled(getActivity(), KEY_LOCK_CLOCK_PACKAGE_NAME)) {
             prefSet.removePreference(mLockClock);
         }
+
+        mClockStyle = (PreferenceScreen) prefSet.findPreference(KEY_STATUS_BAR_CLOCK);
+        updateClockStyleDescription();
     }
 
     @Override
@@ -77,6 +82,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment
     @Override
     public void onResume() {
         super.onResume();
+        updateClockStyleDescription();
     }
 
     @Override
@@ -92,5 +98,17 @@ public class StatusBarSettings extends SettingsPreferenceFragment
         }
         return false;
     }
+
+    private void updateClockStyleDescription() {
+        if (mClockStyle == null) {
+            return;
+        }
+        if (Settings.System.getInt(getContentResolver(),
+               Settings.System.STATUS_BAR_CLOCK, 1) == 1) {
+            mClockStyle.setSummary(getString(R.string.enabled_string));
+        } else {
+            mClockStyle.setSummary(getString(R.string.disabled_string));
+         }
+     }
 }
 
