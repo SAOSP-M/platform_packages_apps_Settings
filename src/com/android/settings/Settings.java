4c44a418168ccb63b905ffdf13d993135aa84003
Randall Rushing <randall.rushing@gmail.com>
2016-02-07 05:00:49 -0800

    Navbar: expose Fling and Smarbar settings [1/2]
    Change-Id: I0e02cb7cd6ab871f08ef049046ea789b6e15e6eb

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -125,4 +125,6 @@ public class Settings extends SettingsActivity {
     public static class QSSettingsActivity extends SettingsActivity { /* empty */ }
     public static class BatteryBarSettingsActivity extends SettingsActivity { /* empty */ }
     public static class NavigationSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class FlingSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class SmartbarSettingsActivity extends SettingsActivity { /* empty */ }
 }
